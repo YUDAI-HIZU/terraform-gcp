ac8fab12cd0c2ed55bbac1655f78756d3136d5e5
hizukayudai@gmail.com <09120912Abcd>
2021-01-26 20:18:38 +0900

    add

---
+++ terraform/variable.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+  default = {
+    tf-sample = ""
+  }
+}
+
+variable "credential" {
+  default = {
+    data = ""
+  }
+}
+
+variable "webhook" {
+  default = {
+    url = ""
+  }
+}
